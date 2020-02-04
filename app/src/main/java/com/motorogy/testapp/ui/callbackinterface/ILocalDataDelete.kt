01c0414bdccdd4fb76ba0f7f86499fb8672287ae
hp <Sherif.gaber.elkomy2012@gmail.com>
2020-02-05 00:28:05 +0300

    Publish

--- app/src/main/java/com/motorogy/testapp/ui/callbackinterface/ILocalDataDelete.kt
+++
@@ -1,7 +0,0 @@
-package com.motorogy.testapp.ui.callbackinterface
-
-import com.motorogy.testapp.data.db.entity.ReceipEntity
-
-interface ILocalDataDelete {
-    fun removeReceipEntityItem(itemReceipEntity: ReceipEntity, lstItem: List<ReceipEntity>, position: Int)
-}
